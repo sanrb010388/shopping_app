7229493f8a953eedd908acdd530b7f9c02f1e68b
sanrb010388 <99805362+sanrb010388@users.noreply.github.com>
2022-06-13 14:25:22 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:shopping_app/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/widgets/CustomButton.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String name;
+  final GestureTapCallback? onTap;
+
+  CustomButton({required this.name, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      child: Text(name),
+    );
+  }
+}
